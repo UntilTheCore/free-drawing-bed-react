d392034bd8f75f02edeea2f6f5b2ff956a5e4286
untilthecore <814898862@qq.com>
2021-09-17 17:51:16 +0800

    处理刷新页面用户信息丢失问题

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,13 @@ import Header from "components/Header";
 import Footer from "components/Footer";
 import Loading from "components/Loading";
 import Center from "components/Center";
+import { useStores } from "store";
 
 function App() {
+  const { userStore } = useStores();
+  // 在页面每次加载时，都主动获取一次用户信息，防止用户强刷后页面用户信息丢失。
+  userStore.pollUser();
+
   return (
     <Router>
       <div>
